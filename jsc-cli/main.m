d42ceb446048d340a5ad8982f8a635569fb6b1e1
winter <csf178@gmail.com>
2022-08-03 21:25:19 +0800

    Initial Commit

---
+++ jsc-cli/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jsc-cli
+//
+//  Created by 程劭非 on 2022/8/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
