ebc1b1c951c9c0faf9e015507a4caab73b410e63
winter <csf178@gmail.com>
2022-08-06 15:58:49 +0800

    feat:event loop & macro task queue

--- jsc-cli/main.m
+++ jsc-cli/main.m
@@ -6,11 +6,62 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <JavaScriptCore/JavaScriptCore.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        JSContext* context = [[JSContext alloc] init];
+        
+        NSMutableArray * macroTaskQueue = [[NSMutableArray alloc] init];
+        NSConditionLock* condLock = [[NSConditionLock alloc] initWithCondition:0];
+        
+        context[@"console"] = [JSValue valueWithNewObjectInContext:context];
+        context[@"console"][@"log"] = ^(JSValue* s) {
+            NSLog(@"%@", [s toString]);
+        };
+        
+        context[@"setTimeout"] = ^(JSValue* f, JSValue* duration) {
+            NSThread* timer = [[NSThread alloc]initWithBlock:^{
+                [NSThread sleepForTimeInterval:[duration toDouble] / 1000];
+                
+                [condLock lock];
+                
+                [macroTaskQueue addObject:f];
+                
+                [condLock unlockWithCondition:1];
+                //[f callWithArguments:@[]];
+            }];
+            [timer start];
+        };
+        
+        NSThread* scanner = [[NSThread alloc]initWithBlock:^{
+            char sourceCode[1024];
+            while(scanf("%[^\n]", sourceCode) != -1) {
+                getchar();
+                [condLock lock];
+                
+                [macroTaskQueue addObject:[NSString stringWithUTF8String: sourceCode]];
+                
+                [condLock unlockWithCondition:1];
+            }
+        }];
+        [scanner start];
+
+        while(true) { //Event Loop
+            [condLock lockWhenCondition:1];
+            for (id task in macroTaskQueue){
+                if([task isKindOfClass:JSValue.class])
+                    [task callWithArguments:@[]];
+                if([task isKindOfClass:NSString.class]) {
+                    JSValue* result = [context evaluateScript:task];
+                    NSLog(@"%@", [result toString]);
+                }
+            }
+            [macroTaskQueue removeAllObjects];
+            [condLock unlockWithCondition:0];
+        }
+
     }
     return 0;
 }
